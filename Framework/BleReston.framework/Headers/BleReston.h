13804f745a0907ef1767248952a5134265e3190a
Shawsan@sleepace.net <Shawsan@sleepace.net>
2021-10-21 16:45:06 +0800

    build 1.0.0!

---
+++ Framework/BleReston.framework/Headers/BleReston.h
@@ -0,0 +1,28 @@
+//
+//  BleReston.h
+//  BleReston
+//
+//  Created by San on 5/12/2019.
+//  Copyright © 2019 Martin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "SLPHistoryData.h"
+#import "SLPBLEDef.h"
+#import "SLPOriginalDataALG.h"
+#import "SLPBLEManager.h"
+#import "SLPBLEManager+Scan.h"
+#import "SLPBLEManager+Reston.h"
+#import "SLPRestonDef.h"
+
+#define kManagerVersion  @"3.0.1";
+
+//! Project version number for BleReston.
+FOUNDATION_EXPORT double BleRestonVersionNumber;
+
+//! Project version string for BleReston.
+FOUNDATION_EXPORT const unsigned char BleRestonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BleReston/PublicHeader.h>
+
+
---
+++ Framework/BleReston.framework/Headers/RestonAutoCollection.h
@@ -0,0 +1,21 @@
+//
+//  RestonAutoCollection.h
+//  SDK
+//
+//  Created by San on 25/2/2020.
+//  Copyright © 2020 Martin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RestonAutoCollection : NSObject
+@property (nonatomic,assign) UInt8 valid;
+@property (nonatomic,assign) UInt8 hour;
+@property (nonatomic,assign) UInt8 minute;
+@property (nonatomic,assign) UInt8 repeat;
+
+@end
+
+NS_ASSUME_NONNULL_END
